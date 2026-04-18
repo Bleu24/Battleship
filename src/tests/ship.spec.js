5af4053f71de33ac78af3a047c9c2a0e141458e7
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-18 20:59:28 +0800

    feat: add isSunk test functions inside Ship suite

--- src/tests/ship.spec.js
+++ src/tests/ship.spec.js
@@ -1,11 +1,15 @@
 import { Ship } from "../classes/Ship";
 
 describe("Ship suite", () => {
-  const ship = new Ship();
+  const ship = new Ship(3);
 
   test("Hit Function", () => {
     ship.hit();
     expect(ship.timesHit).toBe(1);
+  });
+
+  test("isSunk Function", () => {
+    expect(ship.isSunk()).toBeFalsy();
   });
 
   test("Hit Function", () => {
@@ -13,10 +17,17 @@ describe("Ship suite", () => {
     expect(ship.timesHit).toBe(2);
   });
 
+  test("isSunk Function", () => {
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
   test("Hit Function", () => {
     ship.hit()
     expect(ship.timesHit).toBe(3);
   });
 
+  test("isSunk Function", () => {
+    expect(ship.isSunk()).toBeTruthy();
+  });
 
 });
