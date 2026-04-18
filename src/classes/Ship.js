f40a2d721b4f8f53fcdcab10454c46e59f870a11
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-18 20:56:11 +0800

    feat: add constructor param length for health and add isSunk for checking health

--- src/classes/Ship.js
+++ src/classes/Ship.js
@@ -3,9 +3,15 @@ export class Ship {
   length;
   timesHit = 0;
 
-  constructor() { }
+  constructor(length = 0) {
+    this.length = length;
+  }
 
   hit() {
     this.timesHit++;
+  }
+
+  isSunk() {
+    return this.timesHit === this.length;
   }
 }
