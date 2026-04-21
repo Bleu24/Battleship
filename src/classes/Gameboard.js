2ae9672335097b21e86e374fedeec535bd9dcb47
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-21 23:58:49 +0800

    feat: add state object to each cell

--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -1,33 +1,42 @@
+import { Ship } from "./Ship.js";
+
 export class Gameboard {
   constructor(size = 10) {
-    this.board = Array.from({ length: size }, () => Array(size).fill(null));
+    this.board = Array.from({ length: size }, () =>
+      Array.from({ length: size }, () => ({ ship: null, shot: null })),
+    );
   }
 
   place(ship, x, y, orientation) {
-    this.board[x][y] = ship;
+    this.board[x][y].ship = ship;
 
     switch (orientation) {
       case "horizontal":
         for (let i = 0; i < ship.length; i++) {
-          this.board[x][i] = ship;
+          this.board[x][i].ship = ship;
         }
         break;
       case "vertical":
         for (let i = 0; i < ship.length; i++) {
-          this.board[i][y] = ship;
+          this.board[i][y].ship = ship;
         }
         break;
     }
   }
 
   isOccupied(x, y) {
-    return this.board[x][y] !== null;
+    return this.board[x][y].ship !== null;
   }
 
   receiveAttack(x, y) {
-    const ship = this.board[x][y];
-    ship.hit();
+    const cell = this.board[x][y];
 
-    return true;
+    if (cell.shot === "miss" || cell.shot === "hit") return;
+
+    if (cell.ship && cell.ship instanceof Ship) {
+      cell.ship.hit();
+      cell.shot = "hit";
+      return true;
+    } else cell.shot = "miss";
   }
 }
