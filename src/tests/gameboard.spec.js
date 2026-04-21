6866dbe370366bb4165b29493ebd13ac844ade56
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-21 23:10:05 +0800

    feat: add receive attack test and test receve attack function from different coords

--- src/tests/gameboard.spec.js
+++ src/tests/gameboard.spec.js
@@ -2,9 +2,12 @@ import { Ship } from "../classes/Ship";
 import { Gameboard } from "../classes/Gameboard.js";
 
 describe("Gameboard Suite", () => {
+  const gameboard = new Gameboard();
+
   test("Occupied cell", () => {
     const gameboard = new Gameboard();
     const carrier = new Ship(5);
+
     gameboard.place(carrier, 0, 0, "horizontal");
 
     expect(gameboard.isOccupied(0, 0)).toBeTruthy();
@@ -12,5 +15,24 @@ describe("Gameboard Suite", () => {
     expect(gameboard.isOccupied(0, 2)).toBeTruthy();
     expect(gameboard.isOccupied(0, 3)).toBeTruthy();
     expect(gameboard.isOccupied(0, 4)).toBeTruthy();
+  });
+
+  test("Receive attack", () => {
+    const carrier = new Ship(5);
+
+    gameboard.place(carrier, 0, 0, "horizontal");
+
+    expect(gameboard.receiveAttack(0, 0)).toBeTruthy();
+    expect(carrier.getHealth()).toBe(4);
+  });
+
+  test("Receive attack (same ship, different coords", () => {
+    const frigate = new Ship(4);
+    gameboard.place(frigate, 0, 0, "vertical");
+
+    // initial hit (successful)
+    gameboard.receiveAttack(0, 0); // health should be 3
+    gameboard.receiveAttack(3, 0);
+    expect(frigate.getHealth()).toBe(2);
   });
 });
