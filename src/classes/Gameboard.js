fad0afc4d49e9db64a233fbc9c9b7bf189272990
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-21 20:15:52 +0800

    feat: add place and isOccupied functions
    change constructor to use size default parameter for grid allocation

--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -1,9 +1,26 @@
 export class Gameboard {
+  constructor(size = 10) {
+    this.board = Array.from({ length: size }, () => Array(size).fill(null));
+  }
 
-  size = 10;
-  board = Array.from({ length: this.size }, () => Array(this.size).fill(null));
+  place(ship, x, y, orientation) {
+    this.board[x][y] = ship;
 
-  constructor() {
+    switch (orientation) {
+      case "horizontal":
+        for (let i = 0; i < ship.length; i++) {
+          this.board[x][i] = ship;
+        }
+        break;
+      case "vertical":
+        for (let i = 0; i < ship.length; i++) {
+          this.board[i][y] = ship;
+        }
+        break;
+    }
+  }
 
+  isOccupied(x, y) {
+    return this.board[x][y] !== null;
   }
 }
