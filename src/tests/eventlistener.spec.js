e2693d8e6ed91baf6a4679d5cc3f63718a166965
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-23 16:28:07 +0800

    feat: add event listener test
    add hit event test by assigning message to result

---
+++ src/tests/eventlistener.spec.js
@@ -0,0 +1,17 @@
+import { EventListener } from "../classes/EventListener.js";
+
+describe("Event Listener", () => {
+
+  test("Hit event", () => {
+    let result = null;
+    EventListener.subscribe("ship:hit", (msg) => {
+      result = msg;
+    });
+
+    EventListener.emit("ship:hit", "ship hit successfully!");
+
+    expect(result).toBe("ship hit successfully!");
+  });
+
+  expect()
+});
