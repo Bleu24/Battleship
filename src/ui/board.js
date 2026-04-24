e4d0fc1b6220c00d21570c56f63e2c0303ce869e
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-24 15:36:24 +0800

    feat: add createBoard util for ui

---
+++ src/ui/board.js
@@ -0,0 +1,14 @@
+export const createBoard = () => {
+  const container = document.createElement("div");
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.dataset.x = i;
+      cell.dataset.y = j;
+
+      container.appendChild(cell);
+    }
+  }
+};
