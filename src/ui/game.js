fbff10c52665d506268f7693e5995fe3dd094057
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-25 21:13:38 +0800

    feat: create p1 board using createBoard func

--- src/ui/game.js
+++ src/ui/game.js
@@ -1,3 +1,13 @@
+import { createBoard } from "./board.js";
+
 export const Home = (function () {
   const bg = document.createElement("div");
+  const p1_board = createBoard();
+
+  p1_board.className = "p1 board";
+  bg.className = "app";
+
+  bg.appendChild(p1_board);
+
+  return bg;
 })();
