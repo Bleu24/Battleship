e50516cdd663ae915f3e1720e87cf3bb2f094790
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-27 22:02:45 +0800

    feat: add Home module

---
+++ src/ui/home.js
@@ -0,0 +1,28 @@
+import { Game } from "./game.js";
+import { engine } from "../core/engine.js";
+
+export const Home = (function () {
+  const container = document.createElement("div");
+  const heroTitle = document.createElement("h1");
+  const hero = document.createElement("div");
+  const start = document.createElement("button");
+
+  container.className = "app";
+  heroTitle.className = "hero__title";
+  hero.className = "hero";
+  start.className = "hero__start";
+
+  heroTitle.textContent = "BATTLESHIP";
+  start.textContent = "New Game";
+
+  container.appendChild(hero);
+  hero.append(heroTitle, start);
+
+  start.addEventListener("click", () => {
+    hero.remove();
+    container.appendChild(Game);
+    engine.start();
+  });
+
+  return container;
+})();
