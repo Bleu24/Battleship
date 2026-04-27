fc59805d98d190d755467239a49e2daf7cc1abea
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-27 22:01:17 +0800

    feat: add ships property in game

--- src/core/engine.js
+++ src/core/engine.js
@@ -4,10 +4,21 @@ import { Player } from "../classes/Player.js";
 import { Ship } from "../classes/Ship.js";
 
 export const engine = (function () {
-  const game = { player1: null, player2: null };
+  const game = {
+    player1: null,
+    player2: null,
+    ships: {
+      carrier: 5,
+      battleship: 4,
+      cruiser: 3,
+      submarine: 3,
+      destroyer: 2,
+    },
+  };
 
   const start = () => {
-    players.push(new Player("Breezus"), new Player("AI"));
+    game.player1 = new Player("Breezus");
+    game.player2 = new Player("AI");
 
   };
 
