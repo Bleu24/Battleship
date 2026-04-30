afc21a7545cc3111cfdb8e3e59e99fc25a086bd2
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-04-30 14:23:07 +0800

    feat: add player 2 board

--- src/ui/game.js
+++ src/ui/game.js
@@ -1,9 +1,14 @@
 import { createBoard } from "./board.js";
 
 export const Game = (function () {
+  const stage = document.createElement("div");
   const p1_board = createBoard();
+  const p2_board = createBoard();
 
+  stage.className = "stage";
   p1_board.className = "p1 board";
+  p2_board.className = "p2 board";
 
-  return p1_board;
+  stage.append(p1_board, p2_board);
+  return stage;
 })();
