f47e33d4f37ccad1e6d5273c260256ad30b817a8
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-05-19 21:33:42 +0800

    feat: add board classname to container

--- src/ui/board.js
+++ src/ui/board.js
@@ -1,5 +1,6 @@
 export const createBoard = () => {
   const container = document.createElement("div");
+  container.className = "board";
 
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
