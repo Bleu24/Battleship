92558c6384f287babdc23fb2856263a8a9637609
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-05-25 13:57:41 +0800

    feat: add highlight and undo highlight helper for hover on player board

--- src/ui/board.js
+++ src/ui/board.js
@@ -1,4 +1,6 @@
-export const createBoard = () => {
+import { GameService } from "../services/GameService.js";
+
+export const createBoard = (hover = false) => {
   const container = document.createElement("div");
   container.className = "board";
 
@@ -12,6 +14,102 @@ export const createBoard = () => {
       container.appendChild(cell);
     }
   }
+
+  const highlight = (cell, orientation, shipLength) => {
+    const anchor = { x: parseInt(cell.dataset.x), y: parseInt(cell.dataset.y) };
+    const cells = [];
+
+    if (orientation === "horizontal") {
+      for (let i = 0; i < shipLength; i++) {
+        const c = container.querySelector(
+          `.cell[data-x="${anchor.x}"][data-y="${anchor.y + i}"]`
+        );
+        if (!c) break;
+        cells.push(c);
+      }
+
+      for (const cell of cells) {
+        if (cells.length !== shipLength) {
+          cell.style.backgroundColor = "red";
+        } else {
+          cell.style.backgroundColor = "gray";
+        }
+      }
+    } else if (orientation === "vertical") {
+      for (let i = 0; i < shipLength; i++) {
+        const c = container.querySelector(
+          `.cell[data-x="${anchor.x + i}"][data-y="${anchor.y}"]`,
+        );
+        if (!c) break;
+        cells.push(c);
+      }
+    }
+    for (const cell of cells) {
+      if (cells.length !== shipLength) {
+        cell.style.backgroundColor = "red";
+      } else {
+        cell.style.backgroundColor = "gray";
+      }
+    }
+  };
+
+  const undoHighlight = (cell, orientation, shipLength) => {
+    const anchor = { x: parseInt(cell.dataset.x), y: parseInt(cell.dataset.y) };
+    const cells = [];
+
+    if (orientation === "horizontal") {
+      for (let i = 0; i < shipLength; i++) {
+        const c = container.querySelector(
+          `.cell[data-x="${anchor.x}"][data-y="${anchor.y + i}"]`
+        );
+        if (!c) break;
+        cells.push(c);
+      }
+
+      for (const cell of cells) {
+        cell.style.backgroundColor = "black";
+      }
+    } else if (orientation === "vertical") {
+      for (let i = 0; i < shipLength; i++) {
+        const c = container.querySelector(
+          `.cell[data-x="${anchor.x + i}"][data-y="${anchor.y}"]`,
+        );
+        if (!c) break;
+        cells.push(c);
+      }
+    }
+    for (const cell of cells) {
+      cell.style.backgroundColor = "white";
+    }
+  };
+
+  container.addEventListener("pointerover", (e) => {
+    if (hover) {
+      const cell = e.target.closest(".cell");
+      const orientation =
+        document.querySelector("[data-orientation]")?.dataset.orientation;
+      const ship = document.querySelector("[data-selected=\"true\"]")?.dataset
+        .ship;
+      const shipLength = ship ? GameService.getShipsDict()[ship] : 0;
+
+      if (!cell) return;
+      highlight(cell, orientation, shipLength);
+    }
+  });
+
+  container.addEventListener("pointerout", (e) => {
+    if (hover) {
+      const cell = e.target.closest(".cell");
+      const orientation =
+        document.querySelector("[data-orientation]")?.dataset.orientation;
+      const ship = document.querySelector("[data-selected=\"true\"]")?.dataset
+        .ship;
+      const shipLength = ship ? GameService.getShipsDict()[ship] : 0;
+
+      if (!cell) return;
+      undoHighlight(cell, orientation, shipLength);
+    }
+  });
 
   return container;
 };
