6960539e1f71a88d654cce2526e9d8e60b3f201b
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-05-31 23:04:31 +0800

    feat: add eventlistener for Game to update board info and render it properly

--- src/ui/game.js
+++ src/ui/game.js
@@ -1,9 +1,19 @@
-import { createBoard } from "./board.js";
+import { EventListener } from "../classes/EventListener.js";
+import { GameService } from "../services/GameService.js";
+import { createBoard, renderBoard } from "./board.js";
 
+// NOTE: p1 is the human player (for now)
 export const Game = (function () {
   const stage = document.createElement("div");
   const p1_board = createBoard();
   const p2_board = createBoard();
+
+  EventListener.subscribe("game:start", (player) => {
+    const gameboard = GameService.getBoard(player.id);
+    p1_board.id = player.id;
+    p1_board.dataset.playerName = player.name;
+    renderBoard(gameboard, p1_board);
+  });
 
   stage.className = "stage";
   p1_board.className = "p1 board";
