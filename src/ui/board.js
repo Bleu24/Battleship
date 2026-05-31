c0c9c06f025749672c911b62811adbb8a31a40c5
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-05-31 23:03:06 +0800

    fix: re-add dataset property

--- src/ui/board.js
+++ src/ui/board.js
@@ -26,7 +26,7 @@ export const createBoard = (hover = false) => {
     for (let j = 0; j < 10; j++) {
       const cell = document.createElement("div");
       cell.className = "cell";
-      cell.x = i;
+      cell.dataset.x = i;
       cell.dataset.y = j;
 
       container.appendChild(cell);
