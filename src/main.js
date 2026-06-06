092a2bb8963d75affda3281ea0442fa24ffe21dd
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-06-06 15:34:50 +0800

    feat: clears session after reset

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import "normalize.css";
 import "./styles.css";
 import { Home } from "./ui/home.js";
+import { SessionService } from "./services/SessionService.js";
 
+SessionService.clearSession();
 
 document.body.append(Home);
