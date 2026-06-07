c01f535d21438cab62aec146b14663020e49a07e
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-06-07 14:09:43 +0800

    feat: add receive attack interface and setter, getter for game modes

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -47,6 +47,14 @@ export const GameService = (function () {
     gameboard.remove(ship);
   };
 
+  const receiveAttack = (id, x, y) => {
+    const gameboard = Engine.getBoard(id);
+    gameboard.receiveAttack(x, y);
+  };
+
+  const setMode = (mode) => Engine.setMode(mode);
+  const getMode = () => Engine.getMode();
+
 
 
   return {
@@ -61,5 +69,8 @@ export const GameService = (function () {
     getAllShips,
     getShip,
     removeShip,
+    receiveAttack,
+    setMode,
+    getMode
   };
 })();
