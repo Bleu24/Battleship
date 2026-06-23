f8856291165611bc54278f0ca988818d79c65bbb
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-06-23 21:53:05 +0800

    feat: add guard for player array
    populate boards with player info after wizard setup

--- src/ui/game.js
+++ src/ui/game.js
@@ -9,13 +9,31 @@ export const Game = (function () {
   const p1_board = createBoard();
   const p2_board = createBoard();
 
+  // TODO: integrate logic depending on game mode
   EventListener.subscribe("game:start", (player) => {
+
+    if (player instanceof Array) {
+      const [player1, player2] = player;
+      p1_board.id = player1.id;
+      p1_board.dataset.playerName = player1.name;
+      p2_board.id = player2.id;
+      p2_board.dataset.playerName = player2.name;
+
+      const gameboard = GameService.getBoard(player1.id);
+      renderBoard(gameboard, p1_board);
+
+      return;
+    }
+
     if (!(player instanceof Player)) return;
 
     const gameboard = GameService.getBoard(player.id);
     p1_board.id = player.id;
     p1_board.dataset.playerName = player.name;
     renderBoard(gameboard, p1_board);
+
+    EventListener.emit("player:create", p2_board);
+    EventListener.emit("engine:start", { player, p1_board });
   });
 
   stage.className = "stage";
