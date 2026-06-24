6bf1cad6a9f762eaf35173880a29f96a4bfd6bef
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-06-24 11:23:15 +0800

    feat: subscribe to ui:update
    features passDeviceModal to prevent cheating and refactor stage to
    contain boards and turn labels

--- src/ui/game.js
+++ src/ui/game.js
@@ -6,6 +6,8 @@ import { createBoard, renderBoard } from "./board.js";
 // NOTE: p1 is the human player (for now)
 export const Game = (function () {
   const stage = document.createElement("div");
+  const boardsContainer = document.createElement("div");
+  const activePlayerLabel = document.createElement("h2");
   const p1_board = createBoard();
   const p2_board = createBoard();
 
@@ -19,6 +21,8 @@ export const Game = (function () {
       p2_board.id = player2.id;
       p2_board.dataset.playerName = player2.name;
 
+      activePlayerLabel.textContent = `${player1.name}'s Turn`;
+
       const gameboard = GameService.getBoard(player1.id);
       renderBoard(gameboard, p1_board);
 
@@ -30,16 +34,38 @@ export const Game = (function () {
     const gameboard = GameService.getBoard(player.id);
     p1_board.id = player.id;
     p1_board.dataset.playerName = player.name;
+    activePlayerLabel.textContent = `${player}'s Turn!`;
     renderBoard(gameboard, p1_board);
 
     EventListener.emit("player:create", p2_board);
     EventListener.emit("engine:start", { player, p1_board });
   });
 
+  EventListener.subscribe("ui:update", (activePlayer) => {
+    const passDeviceModal = document.createElement("div");
+    passDeviceModal.className = "stage__passModal";
+
+    passDeviceModal.textContent = "Pass Device (Press Space To Continue)!";
+    passDeviceModal.tabIndex = 0;
+    stage.replaceChildren(passDeviceModal, ...stage.children);
+    passDeviceModal.focus();
+
+    passDeviceModal.addEventListener("keyup", (e) => {
+      if (e.key === " " || e.key === "Spacebar") {
+        activePlayerLabel.textContent = `${activePlayer.name}'s Turn`;
+        passDeviceModal.remove();
+      }
+    });
+
+  });
+
   stage.className = "stage";
+  boardsContainer.className = "stage__boardsContainer";
+  activePlayerLabel.className = "stage__label";
   p1_board.className = "p1 board";
   p2_board.className = "p2 board";
 
-  stage.append(p1_board, p2_board);
+  boardsContainer.append(p1_board, p2_board);
+  stage.append(activePlayerLabel, boardsContainer);
   return stage;
 })();
